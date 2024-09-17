e6fafc816708a2dbeff208ac2dd64fa5884a3d0c
AlfredJohnson2002 <s377576@students.cdu.edu.au>
2024-09-17 12:51:23 +1000

    Alfred_V1

---
+++ q1t3.1.py
@@ -0,0 +1,17 @@
+from collections import Counter
+import csv
+
+with open('combined_text.txt', 'r') as file:
+    text = file.read()
+
+words = text.split()
+
+word_counts = Counter(words)
+
+top_words = word_counts.most_common(30)
+
+with open('top_30_words_Using_in_build_functions.csv1', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Word', 'Count'])
+    writer.writerows(top_words)
+
